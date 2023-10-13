5fa7ac3f5d644fd7c0f79c2f79d26e0d5f6e5758
Jonathan Washington <jonathan.n.washington@gmail.com>
2023-10-13 00:03:24 -0400

    and no -z here either

--- corpora/add_morph.py
+++ corpora/add_morph.py
@@ -4,7 +4,7 @@ import sys
 from subprocess import Popen, PIPE
 
 disambig_process = Popen(["vislcg3", "-g", "../apertium-kir.kir.rlx"], stdin=PIPE, stdout=PIPE)
-cgconv_process = Popen(["cg-conv", "-a", "-l" "-z"], stdin=PIPE, stdout=disambig_process.stdin)
+cgconv_process = Popen(["cg-conv", "-a", "-l"], stdin=PIPE, stdout=disambig_process.stdin)
 transducer_process = Popen(["hfst-proc", "-w", "-z", "../kir.automorf.hfst"], stdin=PIPE, stdout=cgconv_process.stdin)
 
 def get_analysis(text):
