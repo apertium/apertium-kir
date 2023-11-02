b4660116bd8884072e60ef044901526f8aa84b84
Jonathan Washington <jonathan.n.washington@gmail.com>
2023-11-02 00:01:09 -0400

    script now works fairly well
    outputs stuff, somme recovery from tokenisation mismatches, adds in [first] subtoken

--- corpora/add_morph.py
+++ corpora/add_morph.py
@@ -24,8 +24,9 @@ def get_analysis(text):
 	sent = []
 	form = ""
 	analyses = []
-	for line in get_disam(text).split('\n'):
-		#print(line)
+	lines = get_disam(text).split('\n')
+	#print(lines)
+	for line in lines:
 		if re.match('"<.*>"', line):
 			if form != "":
 				token = (form, analyses)
@@ -33,24 +34,68 @@ def get_analysis(text):
 				form = ""
 				analyses = []
 			form = line
-		elif re.search("^\t", line):
+		elif re.search("^\t\"", line):
 			analyses.append(line.strip("\t").split())
+		elif re.search("^\t", line):
+			#print(re.search("^\t", line)[0], line.split(), file=sys.stderr)
+			#print(line.split(), file=sys.stderr)
+			these_analyses = line.split()
+			these_analyses.insert(0, re.search("^\t", line)[0]) # add extra tabs to beginning
+			analyses.append(these_analyses)
+	if form != "":
+		#print(line, token)
+		token = (form, analyses)
+		#print(token)
+		sent.append(token)
+		#print(sent)
+	else:
+		print(line, token, file=sys.stderr)
+
 	#print(sent)
 	return sent
 
 def write_sent(sent,analysis):
 	#h = hash_line(sent)
 	#print("["+h+"]\n"+sent+"[/"+h+"]")
-	idxs = [*range(0, len(sent))]
-	for line in zip(sent,analysis,idxs):
-		(file_line, disam_line, idx) = (line[0], line[1], line[2])
-		print(file_line[0], disam_line[0])
+	#idxs = [*range(0, len(sent))]
+	#for line in zip(sent,analysis,idxs):
+	file_idx = 0
+	disam_idx = 0
+	while file_idx < len(sent) and disam_idx < len(analysis):
+		#(file_line, disam_line, idx) = (line[0], line[1], line[2])
+		file_line = sent[file_idx]
+		disam_line = analysis[disam_idx]
+		file_idx += 1
+		disam_idx += 1
+		#print(file_line[0], disam_line[0], file=sys.stderr)
+		#print("INFO: ", disam_line[1], file=sys.stderr)
 		if file_line[0] != disam_line[0]:
 			print("WARNING: mismatch: ", file_line, disam_line, file=sys.stderr)
-			if(sent[idx+1][0] == disam_line[0]):
-				print("NEXT LINE MATCHES:", sent[idx+1][0], disam_line, file=sys.stderr)
+			if(sent[file_idx][0] == disam_line[0]):
+				print("NEXT FILE LINE MATCHES:", sent[file_idx][0], disam_line, file=sys.stderr)
+				disam_idx -= 1
+				print(file_line[0])
+				print("	"+" TOKENISATIONERROR "+" ".join(file_line[1][-1][-2:]))
+			elif(sent[file_idx+1][0] == disam_line[0]):
+				print("NEXTNEXT FILE LINE MATCHES:", sent[file_idx+1][0], disam_line, file=sys.stderr)
+				disam_idx -= 2
+				print(file_line[0])
+				print("	"+" TOKENISATIONERROR "+" ".join(file_line[1][-1][-2:]))
+			elif(file_line[0] == analysis[disam_idx][0]):
+				print("NEXT DISAM LINE MATCHES:", sent[file_idx][0], disam_line, file=sys.stderr)
+				file_idx -= 1
+			else:
+				print(file_line[0])
+				print("	"+" TOKENISATIONERROR "+" ".join(file_line[1][-1][-2:]))
 		else:
-			pass
+			print(disam_line[0])
+			#print(disam_line[1][0], file_line[1][-1][-2:], file=sys.stderr)
+			print("	"+" ".join(disam_line[1][0])+" "+" ".join(file_line[1][-1][-2:]))
+		if len(disam_line[1])>1 and '\t' in disam_line[1][1][0]:
+			#print("INFO: ", disam_line[1], file=sys.stderr)
+			print("\t"+" ".join(disam_line[1][1])+" #")
+		
+	print()
 
 sent = []
 form = ""
@@ -59,22 +104,35 @@ for line in sys.stdin:
 	if re.search("^#", line):
 		if re.search("^# text = ", line):
 			analysis = get_analysis(line.replace("# text = ","").strip('\r\n" '))
+			#print(analysis)
+		print(line.strip('\r\n" '))
 	elif line == "\n" or line == "\r\n":
+		# add last token to sentence (doesn't add to last sentence)
+		token = (form, analyses)
+		sent.append(token)
 		write_sent(sent, analysis)
 		sent = []
 		form = ""
 		analyses = []
 	else:
 		strippedline = line.strip('\r\n')
+		#print("HARGLE", strippedline,"BARGLE")
 		if re.search("<.*>", strippedline):
 			if form != "" and len(analyses) != 0:
 				token = (form, analyses)
+				#print(token)
 				sent.append(token)
 				form = ""
 				analyses = []
 			form = strippedline
 		else:
+			#print(strippedline.split())
 			analyses.append(strippedline.split())
+# add last token to last sentence
+token = (form, analyses)
+sent.append(token)
+# write last sentence, after last line is read in
+write_sent(sent, analysis)
 
 for proc in disambig_process, cgconv_process, transducer_process:
 	proc.kill()
